c7599f9c2c46a0b7a0e9938188c86e99c9bde383
Christopher Doris <github.com/cjdoris>
2020-03-05 14:25:36 +0000

    use Edlib_jll and more type stable

---
+++ README.md
@@ -0,0 +1,37 @@
+# Edlib.jl
+
+Julia bindings for the [edlib string alignment library](https://github.com/Martinsos/edlib), including fast edit distance.
+
+Install: `] install https://github.com/cjdoris/Edlib.jl#master`.
+
+## Examples
+
+```julia
+julia> using BenchmarkTools, Edlib
+
+help?> Edlib
+search: Edlib readlink readline readlines
+
+  Julia bindings for the edlib string alignment library (https://github.com/Martinsos/edlib).
+
+  See edit_distance, alignment_locations, alignment and align for computing alignment information.
+
+  Also see cigar for producing a cigar string from an alignment.
+
+julia> @btime Edlib.edit_distance("missing", "mississippi")
+  1.157 μs (2 allocations: 116 bytes)
+6
+
+julia> @btime Edlib.edit_distance("missing", "mississippi", mode=:infix)
+  1.264 μs (2 allocations: 124 bytes)
+2
+
+julia> @btime Edlib.alignment("missing", "mississippi", mode=:infix)
+  5.477 μs (5 allocations: 368 bytes)
+(distance = 2, range = 1:5, alignment = Edlib.Alignment[Edlib.MATCH, Edlib.MATCH, Edlib.MATCH, Edlib.MATCH, Edlib.MATCH, Edlib.INSERT_TARGET, Edlib.INSERT_TARGET])
+
+julia> @btime Edlib.cigar(ans.alignment, extended=true)
+  423.920 ns (1 allocation: 32 bytes)
+"5=2I"
+
+```
--- src/Edlib.jl
+++ src/Edlib.jl
@@ -1,29 +1,31 @@
 """
 Julia bindings for the [edlib string alignment library](https://github.com/Martinsos/edlib).
 
-The main functionality is provided by [`align`](@ref).
+See [`edit_distance`](@ref), [`alignment_locations`](@ref), [`alignment`](@ref) and [`align`](@ref) for computing alignment information.
+
+Also see [`cigar`](@ref) for producing a cigar string from an alignment.
 """
 module Edlib
 
 using CEnum: @cenum
+using Edlib_jll: libedlib
 
-const edlib = joinpath(dirname(dirname(pathof(Edlib))), "deps", "libedlib.so")
+# C DEFINITIONS (hidden from the user)
 
-const STATUS_OK = 0
-const STATUS_ERROR = 1
+const C_STATUS_OK = 0
+const C_STATUS_ERROR = 1
 
-@cenum AlignMode MODE_GLOBAL MODE_PREFIX MODE_INFIX
-@cenum AlignTask TASK_DISTANCE TASK_LOC TASK_PATH
-@cenum CigarFormat CIGAR_STANDARD CIGAR_EXTENDED
-@enum Alignment::Cuchar ALIGN_MATCH=0 ALIGN_INSERT_TARGET=1 ALIGN_INSERT_QUERY=2 ALIGN_MISMATCH=3
+@cenum CAlignMode C_MODE_GLOBAL C_MODE_PREFIX C_MODE_INFIX
+@cenum CAlignTask C_TASK_DISTANCE C_TASK_LOCATIONS C_TASK_ALIGNMENT
+@cenum CigarFormat C_CIGAR_STANDARD C_CIGAR_EXTENDED
 
-const EqualityPair = Tuple{Cchar, Cchar}
+const CEqualityPair = Tuple{Cchar, Cchar}
 
 struct CAlignConfig
     k :: Cint
-    mode :: AlignMode
-    task :: AlignTask
-    additional_equalities_ptr :: Ptr{EqualityPair}
+    mode :: CAlignMode
+    task :: CAlignTask
+    additional_equalities_ptr :: Ptr{CEqualityPair}
     additional_equalities_length :: Cint
 end
 
@@ -33,114 +35,173 @@ struct CAlignResult
     end_locations :: Ptr{Cint}
     start_locations :: Ptr{Cint}
     num_locations :: Cint
-    alignment :: Ptr{Alignment}
+    alignment :: Ptr{Cuchar}
     alignment_length :: Cint
     alphabet_length :: Cint
 end
 
-struct AlignConfig
-    max_distance :: Union{Int, Missing}
-    mode :: AlignMode
-    task :: AlignTask
-    equalities :: Vector{EqualityPair}
-end
+# JULIA BINDINGS
 
-"""
-    AlignConfig([opts...])
-
-Configuration options for [`align`](@ref). Takes the same keyword options.
-"""
-function AlignConfig(; max_distance::Union{Integer,Missing}=missing, mode::Union{Symbol,AlignMode}=MODE_GLOBAL, locations::Bool=false, alignment::Bool=false, equalities=[])
-    ismissing(max_distance) || max_distance ≥ 0 || error("max_distance must be non-negative")
-    if mode isa Symbol
-        mode =
-            mode == :global ? MODE_GLOBAL :
-            mode == :prefix ? MODE_PREFIX :
-            mode == :infix  ? MODE_INFIX  :
-            error("mode must be :global, :prefix or :infix")
-    end
-    task = alignment ? TASK_PATH : locations ? TASK_LOC : TASK_DISTANCE
-    AlignConfig(max_distance, mode, task, equalities)
-end
-
-CAlignConfig(c::AlignConfig) =
-    CAlignConfig(ismissing(c.max_distance) ? -1 : c.max_distance, c.mode, c.task, pointer(c.equalities), length(c.equalities))
-
-"""
-    AlignResult
-
-The result of an alignment. It has the following fields:
-- `edit_distance`: The edit distance, or `missing` if it exceeds `max_distance`.
-- `end_locations`: The end locations, or `missing` if `edit_distance` is `missing`.
-- `start_locations`: The start locations, or `missing` if not requested with `locations` option or `edit_distance` is `missing`.
-- `alignment`: The alignment, or `missing` if not requested with `alignment` option or `edit_distance` is `missing`. See [`cigar`](@ref) for an alternative way to view this.
-- `alphabet_length`: The number of unique characters.
-"""
-struct AlignResult
-    edit_distance :: Union{Int, Missing} # missing if the distance is larger than max_distance
-    end_locations :: Union{Vector{Cint}, Missing} # missing if edit_distance is missing
-    start_locations :: Union{Vector{Cint}, Missing} # missing if edit_distance is missing or not calculated
-    alignment :: Union{Vector{Alignment}, Missing} # missing if edit_distance is missing or not calculated
-    alphabet_length :: Int
-end
-
-# when own=true, this takes ownership of any memory owned by a, so that it does not need to be freed
-function AlignResult(a::CAlignResult; wrap=false, own=false)
-    a.status == STATUS_OK || error("edlib status $(a.status)")
-    @assert a.edit_distance ≥ -1
-    dist = a.edit_distance == -1 ? missing : a.edit_distance
-    endlocs = a.end_locations == C_NULL ? missing : (own || wrap) ? unsafe_wrap(Array, a.end_locations, a.num_locations, own=own) : unsafe_load.(a.end_locations, 1:a.num_locations)
-    startlocs = a.start_locations == C_NULL ? missing : (own || wrap) ? unsafe_wrap(Array, a.start_locations, a.num_locations, own=own) : unsafe_load.(a.start_locations, 1:a.num_locations)
-    alignment = a.alignment == C_NULL ? missing : (own || wrap) ? unsafe_wrap(Array, a.alignment, a.alignment_length, own=own) : unsafe_load.(a.alignment, 1:a.alignment_length)
-    return AlignResult(dist, endlocs, startlocs, alignment, a.alphabet_length)
-end
+@enum Alignment::Cuchar MATCH=0 INSERT_TARGET=1 INSERT_QUERY=2 MISMATCH=3
 
 stringarg(x::AbstractString) = convert(String, x)
 stringarg(x) = convert(Union{Vector{Cchar}, Vector{Cuchar}}, x)
 
-"""
-    align(query, target; [opts...])
-    align(query, target, config)
-
-Align the query and target strings (or vectors of bytes) and return a [`AlignResult`](@ref). Note that the input is treated as unencoded bytes.
-
-The available options are:
-- `max_distance`: The maximum edit distance to compute, if it is exceeded then the edit distance is reported as `missing`.
-- `mode`: One of `:global` (standard edit distance, the default), `:prefix` (gaps after the query don't count), `:infix` (gaps before and after the query don't count).
-- `locations`: If `true`, the `start_locations` is given in the result, otherwise it is `missing`.
-- `alignment`: If `true`, the `alignment` and `start_locations` are given in the result, otherwise they are `missing`.
-- `equalities`: A vector of 2-tuples of characters which are considered equal.
-
-The second form takes the options as a `AlignConfig`, which is constructed from the same keyword options. If you are calling `align` many times with the same options, it may be more efficient to use this form.
-"""
-align(query, target; opts...) =
-    align(query, target, AlignConfig(; opts...))
-
-function align(query, target, config)
+function _align(query, target, max_distance, mode, task, equalities)
+    # convert the query and target to something we can take a pointer from
     querystr = stringarg(query)
     targetstr = stringarg(target)
-    cconfig = CAlignConfig(config)
-    a = ccall((:edlibAlign, edlib), CAlignResult, (Ptr{Cchar}, Cint, Ptr{Cchar}, Cint, CAlignConfig), pointer(querystr), length(querystr), pointer(targetstr), length(targetstr), cconfig)
-    return AlignResult(a)
+    # parse the keyword options into a CAlignConfig
+    if ismissing(max_distance)
+        c_max_distance = -1
+    else
+        c_max_distance = max_distance = convert(Int, max_distance)
+        max_distance ≥ 0 || error("`max_distance` must be non-negative or `missing`")
+    end
+    mode =
+        isa(mode, CAlignMode) ? mode :
+        mode === :global ? C_MODE_GLOBAL :
+        mode === :prefix ? C_MODE_PREFIX :
+        mode === :infix  ? C_MODE_INFIX  :
+        error("`mode` must be an `AlignMode` or one of `:global`, `:prefix`, `:infix`")
+    task = isa(task, Val) ? typeof(task).parameters[1] : task
+    task =
+        isa(task, CAlignTask) ? task :
+        task == :distance  ? C_TASK_DISTANCE  :
+        task == :locations ? C_TASK_LOCATIONS :
+        task == :alignment ? C_TASK_ALIGNMENT :
+        error("`task` must be an `AlignTask` or one of `:distance`, `:locations`, `:alignment`")
+    if isnothing(equalities)
+        c_equalities_ptr = Ptr{CEqualityPair}(C_NULL)
+        c_equalities_num = 0
+    else
+        equalities = convert(Array{CEqualityPair}, equalities)
+        c_equalities_ptr = pointer(equalities)
+        c_equalities_num = length(equalities)
+    end
+    cconfig = CAlignConfig(c_max_distance, mode, task, c_equalities_ptr, c_equalities_num)
+    # call the C function
+    r = ccall((:edlibAlign, libedlib), CAlignResult, (Ptr{Cchar}, Cint, Ptr{Cchar}, Cint, CAlignConfig), pointer(querystr), length(querystr), pointer(targetstr), length(targetstr), cconfig)
+    # check it worked
+    r.status == C_STATUS_OK || error("edlib align returned status $(r.status)")
+    # parse out the results
+    # this is structured to be type-stable (except for possibly returning `missing` when `max_distance` is given)
+    if ismissing(max_distance)
+        @assert 0 ≤ r.edit_distance
+        edit_distance = r.edit_distance
+    elseif r.edit_distance == -1
+        @assert r.end_locations == C_NULL
+        @assert r.start_locations == C_NULL
+        @assert r.alignment == C_NULL
+        return missing
+    else
+        @assert 0 ≤ r.edit_distance ≤ max_distance
+        edit_distance = r.edit_distance
+    end
+    @assert r.end_locations != C_NULL
+    end_locations = unsafe_wrap(Array, r.end_locations, r.num_locations, own=true)
+    end_locations .+= 1
+    if task == C_TASK_DISTANCE
+        @assert r.start_locations == C_NULL
+        start_locations = missing
+    else
+        @assert r.start_locations != C_NULL
+        start_locations = unsafe_wrap(Array, r.start_locations, r.num_locations, own=true)
+        start_locations .+= 1
+    end
+    if task == C_TASK_DISTANCE || task == C_TASK_LOCATIONS
+        @assert r.alignment == C_NULL
+        alignment = missing
+    else
+        @assert r.alignment != C_NULL
+        alignment = unsafe_wrap(Array, Ptr{Alignment}(r.alignment), r.alignment_length, own=true)
+    end
+    return (distance=Int(edit_distance), starts=start_locations, ends=end_locations, alignment=alignment)
 end
 
 """
-    cigar(a; extended=false)
+    align(query, target; ...)
 
-The cigar string of the `Vector{Alignment}` or `AlignResult`.
+Align the `query` and `target` strings. Inputs are as for [`edit_distance`](@ref).
+
+The available options are:
+- `max_distance`: The maximum edit distance to compute. If the actual edit distance is larger, it is reported as `missing`.
+- `mode`: One of `:global` (standard edit distance, the default), `:prefix` (gaps after the query don't count), `:infix` (gaps before and after the query don't count).
+- `equalities`: A vector of 2-tuples of characters which are considered equal. If you are calling `align` many times, this should be a `Vector{Tuple{UInt8,UInt8}}` to avoid conversion.
+"""
+function align(query, target, task=Val(C_TASK_ALIGNMENT); max_distance=missing, mode=C_MODE_GLOBAL, equalities=nothing)
+    return _align(query, target, max_distance, mode, task, equalities)
+end
+
+"""
+    edit_distance(query, target; max_distance=nothing, mode=:global, equalities=nothing)
+
+The edit distance between `query` and `target` strings (or vectors of bytes). Note that the input is treated as unencoded bytes.
+
+The available options are:
+- `max_distance`: The maximum edit distance to compute. If the actual edit distance is larger, it is reported as `missing`.
+- `mode`: One of `:global` (standard edit distance, the default), `:prefix` (gaps after the query don't count), `:infix` (gaps before and after the query don't count).
+- `equalities`: A vector of 2-tuples of characters which are considered equal. If you are calling `align` many times, this should be a `Vector{Tuple{UInt8,UInt8}}` to avoid conversion.
+
+If `max_distance` is given and the actual edit distance is larger, `missing` is returned.
+"""
+function edit_distance(query, target; opts...)
+    r = align(query, target, Val(C_TASK_DISTANCE); opts...)
+    return ismissing(r) ? missing : r.distance
+end
+
+"""
+    alignment_end_locations(query, target; opts...)
+
+The edit distance and vector of end locations of optimal alignments. Inputs are as for [`edit_distance`](@ref).
+"""
+function alignment_end_locations(query, target; opts...)
+    r = align(query, target, Val(C_TASK_DISTANCE); opts...)
+    return ismissing(r) ? missing : (distance=r.distance, ends=r.ends)
+end
+
+"""
+    alignment_locations(query, target; opts...)
+
+The edit distance and vectors of start and end locations of optimal alignments. Inputs are as for [`edit_distance`](@ref).
+"""
+function alignment_locations(query, target; opts...)
+    r = align(query, target, Val(C_TASK_LOCATIONS); opts...)
+    return ismissing(r) ? missing : (distance=r.distance, starts=r.starts, ends=r.ends)
+end
+
+"""
+    alignment(query, target; opts...)
+
+The edit distance, and the range and character alignments of an optimal alignment. Inputs are as for [`edit_distance`](@ref).
+"""
+function alignment(query, target; opts...)
+    r = align(query, target, Val(C_TASK_ALIGNMENT); opts...)
+    return ismissing(r) ? missing : (distance=r.distance, range=Int(r.starts[1]):Int(r.ends[1]), alignment=r.alignment)
+end
+
+"""
+    cigar(a::Vector{Alignment}; extended=false)
+    cigar(query, target; extended=false, ...)
+
+The cigar string of the given `Vector{Alignment}` (e.g. as returned from `align` or `alignment`).
 
 It consists of pairs of multiplicities followed by a character `I` (inerstion), `D` (deletion), `=`/`X` (match/mismatch, extended only), `M` (match/mismatch).
+
+The second form computes the cigar string for the alignment of `query` and `target`. The inputs are as for [`edit_distance`](@ref).
 """
 function cigar(alignment::Vector{Alignment}; extended::Bool=false)
-    format = extended ? CIGAR_EXTENDED : CIGAR_STANDARD
-    c = ccall((:edlibAlignmentToCigar, edlib), Cstring, (Ptr{Alignment}, Cint, CigarFormat), pointer(alignment), length(alignment), format)
+    format = extended ? C_CIGAR_EXTENDED : C_CIGAR_STANDARD
+    c = ccall((:edlibAlignmentToCigar, libedlib), Cstring, (Ptr{Alignment}, Cint, CigarFormat), pointer(alignment), length(alignment), format)
     # TODO: is it possible to `unsafe_wrap` a string to avoid this allocation+free?
     r = unsafe_string(c)
     Base.Libc.free(c)
     return r
 end
 
-cigar(r::AlignResult; opts...) =
-    ismissing(r.alignment) ? error("alignment not computed, perhaps call `align` again with option `alignment=true`") : cigar(r.alignment; opts...)
+function cigar(query, target; extended::Bool=false, opts...)
+    r = alignment(query, target; opts...)
+    return ismissing(r) ? missing : cigar(r.alignment, extended=extended)
+end
 
 end # module
